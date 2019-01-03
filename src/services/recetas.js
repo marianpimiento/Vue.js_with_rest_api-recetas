f66c8c3181948254b008a0a9aa35bfd1ffa0054b
marianpimiento <ing.mariapimiento@gmail.com>
2019-01-02 20:28:45 -0500

    Services creados: config.js y recetas.js

---
+++ src/services/recetas.js
@@ -0,0 +1,20 @@
+import { http } from './config'
+
+export default{
+  
+  get: () => {
+    return http.get('recetas')
+  },
+
+  create: (receta) => {
+    return http.post('receta/'+receta.idrecetas, receta)
+  },
+
+  update: (receta) => {
+    return http.put('receta/'+receta.idrecetas, receta)
+  },
+
+  delete: (receta) => {
+    return http.delete('receta/'+receta.idrecetas)
+  }
+}
