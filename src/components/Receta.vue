101f1fc3476fc10053f1a46ae3b3477634542308
marianpimiento <ing.mariapimiento@gmail.com>
2019-01-03 22:26:35 -0500

    Diseño con estilos bootstrap 4.0

--- src/components/Receta.vue
+++ src/components/Receta.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
 
-    <nav>
-      <div class="nav-wrapper blue darken-1">
-        <a href="#" class="brand-logo center">Recetas de cocina</a>
+    <!-- <div class="jumbotron" style="background-color:#fca758 !important; padding: 0em inherit !important;"> -->
+    <div class="jumbotron" style="
+        background-image: url(https://static.wixstatic.com/media/441212_640c1cb265cc4e42b8375c30206b4ba5~mv2.jpg);
+        background-size: cover;
+        padding: 20px !important;">
+      <div class="container align-center">
+        <h3 class="display-4 text-center text-white"><b>Recetas de Cocina</b></h3>
       </div>
-    </nav>
+    </div>
+
+    <!-- '../assets/background_comida.jpg' -->
 
     <div class="container">
       <ul>
@@ -14,7 +20,7 @@
         </li>
       </ul>
 
-      <form @submit.prevent="guardar">
+      <!-- <form @submit.prevent="guardar">
 
           <label>Nombre</label>
           <input type="text" v-model="receta.nombre">
@@ -29,19 +35,87 @@
 
           <button class="waves-effect waves-light btn-small">Guardar<i class="material-icons left">save</i></button>
 
-      </form>
+      </form> -->
 
+      <div class="card">
+      <h5 class="card-header">Crear receta</h5>
+
+      <div class="card-body">
+        <form @submit.prevent="guardar">
+
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="inputNombre">Nombre</label>
+              <input type="text" class="form-control" id="inputNombre" v-model="receta.nombre">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputTipo">Tipo de plato</label>
+              <select id="inputTipo" class="form-control" v-model="receta.tipoplato">
+                <option selected></option>
+                <option>Sopa</option>
+                <option>Ensalada</option>
+                <option>Postre</option>
+                <option>Otro</option>
+              </select>
+            </div>
+            <div class="form-group col-md-2">
+              <label for="inputPorciones">Porciones</label>
+              <input type="text" class="form-control" id="inputPorciones" v-model="receta.porciones">
+            </div>
+          </div>
+
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="textareaIngredientes">Ingredientes</label>
+              <textarea class="form-control" id="textareaIngredientes" rows="3" v-model="receta.ingredientes"></textarea>
+            </div>
+            <div class="form-group col-md-6">
+              <label for="textareaPreparacion">Preparación</label>
+              <textarea class="form-control" id="textareaPreparacion" rows="3" v-model="receta.preparacion"></textarea>
+            </div>
+          </div>
+
+          <button type="submit" class="btn btn-info">Guardar</button>
+        </form>
+      </div>
+
+    </div>
+  
+      <br>
       <form @submit.prevent="listar">
 
-          <label>Buscar por nombre</label>
-          <input type="text" v-model="txtBusqueda">
-
-          <button class="waves-effect waves-light btn-small">Buscar</button>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" aria-label="Buscar por nombre" aria-describedby="basic-addon2" v-model="txtBusqueda">
+            <!-- <input type="text" class="form-control" aria-label="Buscar por nombre" aria-describedby="basic-addon2" v-model="txtBusqueda"  @input="listar"> -->
+            <div class="input-group-append">
+              <button class="btn btn-info" type="button">Buscar</button>
+            </div>
+          </div>
 
       </form>
 
+      <br><br>
 
-      <table>
+
+      <div class="card border-dark mb-3" v-for="receta of recetas" :key="receta.idrecetas">
+        <div class="card-header " style="background-color: #bbf7e5;">
+          {{receta.nombre}} ({{receta.porciones}} porciones) - {{receta.tipoplato}}
+          <button @click="eliminar(receta)" type="button" class="btn btn-outline-dark float-right">Borrar <i class="fa fa-home"></i></button>
+          <button @click="editar(receta)" type="button" class="btn btn-outline-dark float-right">Editar <i class="fa fa-home"></i></button>
+         
+        </div>
+        <div class="card-body text-dark">
+          <dl class="row">
+            <dt class="col-sm-3">Ingredientes</dt>
+            <dd class="col-sm-9">{{receta.ingredientes}}</dd>
+
+            <dt class="col-sm-3 text-truncate">Preparación</dt>
+            <dd class="col-sm-9">{{receta.preparacion}}</dd>
+          </dl>
+        </div>
+      </div>
+
+      <!-- <table>
 
         <thead>
           <tr>
@@ -68,7 +142,7 @@
           </tr>
         </tbody>
       
-      </table>
+      </table> -->
 
     </div>
 
