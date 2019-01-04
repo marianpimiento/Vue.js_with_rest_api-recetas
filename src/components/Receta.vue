3cd03642057c89c453dbd10aa0c9b8e86d07e6fd
marianpimiento <ing.mariapimiento@gmail.com>
2019-01-03 23:48:48 -0500

    Adiciones al estilo y estructura de Receta.vue, e inclusion de funcionalidades regresar a la seccion superior al editar y limpiar la busqueda

--- src/components/Receta.vue
+++ src/components/Receta.vue
@@ -2,41 +2,28 @@
   <div id="app">
 
     <!-- <div class="jumbotron" style="background-color:#fca758 !important; padding: 0em inherit !important;"> -->
+    
+    <!-- Seccion del banner principal para el titulo -->
     <div class="jumbotron" style="
         background-image: url(https://static.wixstatic.com/media/441212_640c1cb265cc4e42b8375c30206b4ba5~mv2.jpg);
         background-size: cover;
         padding: 20px !important;">
       <div class="container align-center">
-        <h3 class="display-4 text-center text-white"><b>Recetas de Cocina</b></h3>
+        <h1 class="display-4 text-center text-white"><b>Recetas de Cocina</b></h1>
       </div>
     </div>
+    <!-- https://www.aspic.edu.mx/wp-content/uploads/2018/03/aspic-cocina-mexicana-tradicional.jpg -->
 
-    <!-- '../assets/background_comida.jpg' -->
-
+    <a name="arriba"></a>
     <div class="container">
+
       <ul>
         <li v-for="(erro,index) of errors" :key="index">
           Campo <b>{{erro.field}}</b> - {{erro.defaultMessage}}
         </li>
       </ul>
 
-      <!-- <form @submit.prevent="guardar">
-
-          <label>Nombre</label>
-          <input type="text" v-model="receta.nombre">
-          <label>Tipo de Plato</label>
-          <input type="text" v-model="receta.tipoplato">
-          <label>Porciones</label>
-          <input type="text" v-model="receta.porciones">
-          <label>Ingredientes</label>
-          <input type="text" v-model="receta.ingredientes">
-          <label>Preparacion</label>
-          <input type="text" v-model="receta.preparacion">
-
-          <button class="waves-effect waves-light btn-small">Guardar<i class="material-icons left">save</i></button>
-
-      </form> -->
-
+      <!-- Seccion de formulario para creacion de receta -->
       <div class="card">
       <h5 class="card-header">Crear receta</h5>
 
@@ -52,9 +39,11 @@
               <label for="inputTipo">Tipo de plato</label>
               <select id="inputTipo" class="form-control" v-model="receta.tipoplato">
                 <option selected></option>
-                <option>Sopa</option>
-                <option>Ensalada</option>
+                <option>Entrada</option>
+                <option>Plato fuerte</option>
                 <option>Postre</option>
+                <option>Bocadillo</option>
+                <option>Bebida</option>
                 <option>Otro</option>
               </select>
             </div>
@@ -75,12 +64,19 @@
             </div>
           </div>
 
-          <button type="submit" class="btn btn-info">Guardar</button>
+          <button type="submit" class="btn btn-info float-right">Guardar</button>
         </form>
       </div>
+      </div>
 
-    </div>
-  
+      <br>
+
+      <div class="alert alert-info text-center" role="alert">
+        <h3>Recetas</h3>
+      </div>
+
+    
+      <!-- Seccion de formulario para busqueda de receta por nombre -->
       <br>
       <form @submit.prevent="listar">
 
@@ -88,61 +84,34 @@
             <input type="text" class="form-control" aria-label="Buscar por nombre" aria-describedby="basic-addon2" v-model="txtBusqueda">
             <!-- <input type="text" class="form-control" aria-label="Buscar por nombre" aria-describedby="basic-addon2" v-model="txtBusqueda"  @input="listar"> -->
             <div class="input-group-append">
-              <button class="btn btn-info" type="button">Buscar</button>
+              <button class="btn btn-info" type="submit">Buscar</button>
+              <button @click="limpiar" class="btn btn-outline-info" type="button">Limpiar</button>
             </div>
           </div>
 
       </form>
 
+      {{recetas.length}} recetas encontradas
       <br><br>
 
-
+      <!-- Seccion de presentacion de recetas -->
       <div class="card border-dark mb-3" v-for="receta of recetas" :key="receta.idrecetas">
-        <div class="card-header " style="background-color: #bbf7e5;">
-          {{receta.nombre}} ({{receta.porciones}} porciones) - {{receta.tipoplato}}
-          <button @click="eliminar(receta)" type="button" class="btn btn-outline-dark float-right">Borrar <i class="fa fa-home"></i></button>
-          <button @click="editar(receta)" type="button" class="btn btn-outline-dark float-right">Editar <i class="fa fa-home"></i></button>
-         
+        <div class="card-header">
+        <!-- <div class="card-header " style="background-color: #f5b1af;"> -->
+          <b>{{receta.nombre}} ({{receta.porciones}} porciones) - {{receta.tipoplato}}</b>
+          <button @click="eliminar(receta)" type="button" class="btn btn-outline-dark float-right">Borrar</button>
+          <button @click="editar(receta)" type="button" class="btn btn-outline-dark float-right">Editar</button>
         </div>
         <div class="card-body text-dark">
           <dl class="row">
-            <dt class="col-sm-3">Ingredientes</dt>
-            <dd class="col-sm-9">{{receta.ingredientes}}</dd>
+            <dt class="col-md-3">Ingredientes</dt>
+            <dd class="col-md-9">{{receta.ingredientes}}</dd>
 
-            <dt class="col-sm-3 text-truncate">Preparación</dt>
-            <dd class="col-sm-9">{{receta.preparacion}}</dd>
+            <dt class="col-md-3">Preparación</dt>
+            <dd class="col-md-9">{{receta.preparacion}}</dd>
           </dl>
         </div>
       </div>
-
-      <!-- <table>
-
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Tipo de Plato</th>
-            <th>Porciones</th>
-            <th>Ingredientes</th>            
-            <th>Preparacion</th>
-            <th>Opciones</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr v-for="receta of recetas" :key="receta.idrecetas">
-            <td>{{receta.nombre}}</td>
-            <td>{{receta.tipoplato}}</td>
-            <td>{{receta.porciones}}</td>
-            <td>{{receta.ingredientes}}</td>
-            <td>{{receta.preparacion}}</td>
-            <td>
-              <button @click="editar(receta)" class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
-              <button @click="eliminar(receta)" class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
-            </td>
-          </tr>
-        </tbody>
-      
-      </table> -->
 
     </div>
 
@@ -157,10 +126,10 @@ export default {
   data () {
     return {
       receta: {
-        idrecetas: '',
+        idrecetas: 0,
         nombre: '',
         tipoplato: '',
-        porciones: '',
+        porciones: 0,
         ingredientes: '',
         preparacion: ''
       },
@@ -193,6 +162,8 @@ export default {
           // console.log(e.response.data.errors)
           // this.errors = e.response.data.errors
             console.log(e)
+            // this.errors = e
+            alert('Error: No se ha podido completar la creación')
         })
       } else {
         Receta.update(this.receta).then(response => {
@@ -203,17 +174,19 @@ export default {
         }).catch(e => {
           // console.log(e.response.data.errors)
           // this.errors = e.response.data.errors
-            console.log(e)
+          console.log(e)
+          // this.errors = e
+          alert('Error: No se ha podido realizar la actualización')
         })
       }
     },
 
     editar (receta) {
+      window.location.href = "#arriba";
       this.receta = receta
     },
 
     eliminar (receta) {
-
       if(confirm('¿Desea eliminar la receta?')){
         Receta.delete(receta).then(response => {
           this.listar()
@@ -221,9 +194,17 @@ export default {
         }).catch(e => {
             // this.errors = e.response.data.errors
             console.log(e)
+            // this.errors = e
+            alert('Error: No se ha eliminado la receta')
         })
       }
-    }
+      this.receta = {}
+    },
+
+    limpiar (receta) {
+      this.txtBusqueda = ''
+      this.listar()
+    },
   }
 }
 </script>
